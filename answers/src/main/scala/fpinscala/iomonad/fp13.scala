aefc75f3f12b9503f688cadc8c16a7b4786c6a70
kharole <oleg.kharchenko@tain.com>
2021-01-13 21:26:17 +0200

    13.2

--- answers/src/main/scala/fpinscala/iomonad/fp13.scala
+++ answers/src/main/scala/fpinscala/iomonad/fp13.scala
@@ -1,5 +1,7 @@
 package fpinscala.iomonad
 
+import fpinscala.iomonad
+
 object fp13 {
 
   //13.1
@@ -9,4 +11,17 @@ object fp13 {
     override def flatMap[A, B](a: Free[F, A])(f: A => Free[F, B]): Free[F, B] = IO3.FlatMap(a, f)
   }
 
+  //13.2
+  @annotation.tailrec
+  def runTrampoline[A](a: Free[Function0, A]): A = a match {
+    case IO3.Return(a) => a
+    case IO3.Suspend(s) => s()
+    case IO3.FlatMap(s, f) => {
+      s match {
+        case IO3.Return(a) => runTrampoline(f(a))
+        case IO3.Suspend(r) => runTrampoline(f(r()))
+        case IO3.FlatMap(y, g) => runTrampoline(y flatMap (a => g(a) flatMap f))
+      }
+    }
+  }
 }
