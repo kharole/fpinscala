1769fb47d4974b5f408f6d66f76bff2a572fdfda
kharole <oleg.kharchenko@tain.com>
2021-04-10 12:24:57 +0300

    13.5

--- answers/src/main/scala/fpinscala/iomonad/fp13.scala
+++ answers/src/main/scala/fpinscala/iomonad/fp13.scala
@@ -97,4 +97,24 @@ object fp13 {
 
 
 
+  //13.5
+  import java.nio._
+  import java.nio.channels._
+
+  def read(file: AsynchronousFileChannel,
+           fromPosition: Long,
+           numBytes: Int): Par[Either[Throwable, Array[Byte]]] =
+    Par.async { (cb: Either[Throwable, Array[Byte]] => Unit) =>
+      val buf = ByteBuffer.allocate(numBytes)
+      file.read(buf, fromPosition, (), new CompletionHandler[Integer, Unit] {
+        def completed(bytesRead: Integer, ignore: Unit) = {
+          val arr = new Array[Byte](bytesRead)
+          buf.slice.get(arr, 0, bytesRead)
+          cb(Right(arr))
+        }
+        def failed(err: Throwable, ignore: Unit) =
+          cb(Left(err))
+      })
+    }
+
 }
